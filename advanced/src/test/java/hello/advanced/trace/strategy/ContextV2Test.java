fdeaa0518a6568098e75421ad5ee8d6bf1906351
Haemin Jeong <jhm0828@gmail.com>
2021-11-22 21:28:31 +0900

    전략 패턴 예제 - 파라미터 전달 방식

---
+++ advanced/src/test/java/hello/advanced/trace/strategy/ContextV2Test.java
@@ -0,0 +1,56 @@
+package hello.advanced.trace.strategy;
+
+import hello.advanced.trace.strategy.code.Strategy;
+import hello.advanced.trace.strategy.code.strategy.ContextV2;
+import hello.advanced.trace.strategy.code.strategy.StrategyLogic1;
+import hello.advanced.trace.strategy.code.strategy.StrategyLogic2;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ContextV2Test {
+
+    /**
+     * 전략 패턴 적용 - 파라미터 전달 방식
+     * 1. 클라이언트는 Context를 실행하면서 인자로 Strategy를 전달
+     * 2. context.execute() 실행
+     * 3. context.execute()의 파라미터로 전달된 strategy.call() 실행
+     */
+    @Test
+    void strategyV1() {
+        ContextV2 context = new ContextV2();
+        context.execute(new StrategyLogic1());
+        context.execute(new StrategyLogic2());
+    }
+
+    /**
+     * 전략 패턴 - 파라미터 전달 방식 + 익명 내부 클래스
+     */
+    @Test
+    void strategyV2() {
+        ContextV2 context = new ContextV2();
+        context.execute(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        });
+
+        context.execute(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직2 실행");
+            }
+        });
+    }
+
+    /**
+     * 전략 패턴 - 파라미터 전달 방식 + 람다
+     */
+    @Test
+    void strategyV3() {
+        ContextV2 context = new ContextV2();
+        context.execute(() -> log.info("비즈니스 로직1 실행"));
+        context.execute(() -> log.info("비즈니스 로직2 실행"));
+    }
+}
