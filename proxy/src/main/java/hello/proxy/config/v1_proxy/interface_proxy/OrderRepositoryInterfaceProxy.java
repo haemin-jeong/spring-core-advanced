e787a99c08ff20a329a6362f568db39206310485
Haemin Jeong <jhm0828@gmail.com>
2021-11-28 20:15:31 +0900

    인터페이스가 있는 구현 클래스에 프록시 적용

---
+++ proxy/src/main/java/hello/proxy/config/v1_proxy/interface_proxy/OrderRepositoryInterfaceProxy.java
@@ -0,0 +1,28 @@
+package hello.proxy.config.v1_proxy.interface_proxy;
+
+import hello.proxy.app.v1.OrderRepositoryV1;
+import hello.proxy.trace.TraceStatus;
+import hello.proxy.trace.logtrace.LogTrace;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class OrderRepositoryInterfaceProxy implements OrderRepositoryV1 {
+
+    //프록시에선 보통 내가 실제 호출하는 대상을 target 이라고 한다.
+    private final OrderRepositoryV1 target;
+    private final LogTrace logTrace;
+
+    @Override
+    public void save(String itemId) {
+        TraceStatus status = null;
+        try {
+            status = logTrace.begin("OrderRepository.request()");
+            //target 호출
+            target.save(itemId);
+            logTrace.end(status);
+        } catch (Exception e) {
+            logTrace.exception(status, e);
+            throw e;
+        }
+    }
+}
